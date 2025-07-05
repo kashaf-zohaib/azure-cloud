2811d0221a332f8e0cb49df8fc8b32fb8a992ce3
kashaf-zohaib <kashafzohaib@hotmail.com>
2025-07-04 21:56:58 -0400

    Delete petstore/petstoreapp/src/main/java/com/chtrembl/petstoreapp/service/StorageService.java

--- petstore/petstoreapp/src/main/java/com/chtrembl/petstoreapp/service/StorageService.java
+++
@@ -1,7 +0,0 @@
-package com.chtrembl.petstoreapp.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface StorageService {
-     public String uploadFile(MultipartFile file);
-}
